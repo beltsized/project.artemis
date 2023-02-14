362f3be5e78ddeb4b00be2a76d36eff221027249
catwizard1 <124324348+catwizard1@users.noreply.github.com>
2023-02-14 00:35:08 -0600

    Create info.lua

---
+++ info.lua
@@ -0,0 +1,8 @@
+local info = {}
+
+info['token']    = '' -- no token for u silly!
+info['settings'] = {logLevel = 0}
+info['owners']   = {'459581966509932556', '993823168684896256', '712190258833260584'}
+info['prefix']   = ';;'
+
+return info
